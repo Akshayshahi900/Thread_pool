2b4e9edd49d1d373e4d0d8a5bd751f41e9bd68a9
Akshay shahi <shahiakshay900@gmail.com>
2026-06-22 21:05:13 +0530

    Feat: Add one worker one queue and many jobs implemented along with graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 use std::sync::mpsc;
 use std::thread;
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let handle = thread::spawn(move || {
-        loop {
-            match rx.recv() {
-              Ok(message) => println!("Recieved: {}", message),
-              Err(_) =>{
-                println!("Channel Closed");
-                break;
-              }
-            }
-        }
-    });
-    tx.send("Job 1").unwrap();
-    tx.send("Job 2").unwrap();
-    tx.send("Job 3").unwrap();
+   let (tx , rx) = mpsc::channel::<Job>();
+
+   let handle = thread::spawn(move || {
+    loop{
+      match rx.recv() {
+          Ok(job)=>{
+            println!("Worker got a job");
+            job();
+          }
+          Err(_) =>{
+            println!("Worker shutting down");
+            break;
+          }
+      }
+    }
+   });
+    tx.send(Box::new(||{
+      println!("Job 1 running");
+    })).unwrap();
+
     
+    tx.send(Box::new(||{
+      println!("Job 2 running");
+    })).unwrap();
+
     drop(tx);
+
     handle.join().unwrap();
 }
