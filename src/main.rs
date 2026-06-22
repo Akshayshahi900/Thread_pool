e102c48dc63ba9259ea1c9ecac3319c45ca7e2c1
Akshay shahi <shahiakshay900@gmail.com>
2026-06-22 22:39:12 +0530

    Feat: Add data structures for the workers and threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,43 @@
-use std::sync::mpsc;
-use std::thread;
-
+use std::{
+  sync::{mpsc , Arc , Mutex},
+  thread,
+};
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker{
+  id:usize,
+  thread:std::thread::JoinHandle<()>,
+}
+
+impl Worker{
+  fn new(
+    id:usize , receiver :Arc<Mutex<mpsc::Receiver<Job>>>,
+  ) ->Worker{
+    let thread = thread::spawn(move ||{
+      loop{
+        let message = receiver.lock().unwrap().recv();
+
+        match message{
+          Ok(job) =>{
+            println!("worker {id} got a job");
+            job();
+          }
+          Err(_) =>{
+            println!("Worker {id} shutting down");
+            break;
+          }
+        }
+      }
+    });
+    Worker { id, thread }
+  }
+}
+
+
+struct ThreadPool{
+  workers:Vec<Worker>,
+  sender: mpsc::Sender<Job>,
+}
 
 fn main() {
    let (tx , rx) = mpsc::channel::<Job>();
