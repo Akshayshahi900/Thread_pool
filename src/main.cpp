d9a223f6586c0b2bf50985b4a49e219f4355b7a1
Akshay shahi <shahiakshay900@gmail.com>
2026-07-02 22:33:24 +0530

    Feat: Add a lock free queue using atomic and Compare and Swap

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,77 @@
 #include <functional>
 #include <iostream>
 #include <mutex>
+#include <optional>
 #include <random>
 #include <thread>
+template <typename T, size_t capacity = 1024> class LockFreeDeque {
+private:
+  std::array<T, capacity> buffer_;
+  std::atomic<size_t> top_{0};
+  std::atomic<size_t> bottom_{0};
+
+public:
+  bool push_bottom(T task);
+  std::optional<T> pop_bottom();
+  std::optional<T> steal_top();
+};
+
+template <typename T, size_t capacity>
+bool LockFreeDeque<T, capacity>::push_bottom(T task) {
+  size_t b = bottom_.load(std::memory_order_relaxed);
+
+  buffer_[b % capacity] = std::move(task);
+
+  bottom_.store(b + 1, std::memory_order_release);
+
+  return true;
+}
+
+template <typename T, size_t capacity>
+std::optional<T> LockFreeDeque<T, capacity>::steal_top() {
+  while (true) {
+    size_t t = top_.load(std::memory_order_acquire);
+    size_t b = bottom_.load(std::memory_order_acquire);
+
+    if (t >= b) {
+      return std::nullopt;
+    }
+
+    T task = buffer_[t % capacity];
+    if (top_.compare_exchange_strong(t, t + 1, std::memory_order_acquire,
+                                     std::memory_order_relaxed)) {
+      return task;
+    }
+  }
+}
+
+template <typename T, size_t capacity>
+std::optional<T> LockFreeDeque<T, capacity>::pop_bottom() {
+  size_t b = bottom_.load(std::memory_order_relaxed);
+
+  if (b == 0)
+    return std::nullopt;
+  b--;
+  bottom_.store(b, std::memory_order_relaxed);
+
+  size_t t = top_.load(std::memory_order_acquire);
+
+  if (t > b) {
+    bottom_.store(t, std::memory_order_relaxed);
+    return std::nullopt;
+  }
+
+  T task = buffer_[b % capacity];
+  if (t == b) {
+    if (!top_.compare_exchange_strong(t, t + 1, std::memory_order_acq_rel,
+                                      std::memory_order_relaxed)) {
+      bottom_.store(t + 1, std::memory_order_relaxed);
+      return std::nullopt;
+    }
+    bottom_.store(t + 1, std::memory_order_relaxed);
+  }
+  return task;
+}
 struct Job {
   std::function<void()> task;
 };
@@ -14,8 +83,7 @@ struct Job {
 class Worker {
 public:
   std::thread thread_;
-  std::deque<Job> deque_;
-  std::mutex mutex_;
+  LockFreeDeque<Job> deque_;
   std::condition_variable cv_;
 };
 
